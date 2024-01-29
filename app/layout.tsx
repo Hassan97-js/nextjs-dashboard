835898d0b01cae0158b62a1945e3d558e1e2c933
Hassan97-js <hassan.sweden.dev@gmail.com>
2024-01-29 13:12:33 +0100

    first comiit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import { Inter } from "next/font/google";
+import "@/app/ui/global.css";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
