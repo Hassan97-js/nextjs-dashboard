09d174dc8d6e629d7f1550bcea54e25d2070386b
Hassan97-js <hassan.sweden.dev@gmail.com>
2024-01-29 13:24:46 +0100

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "@/app/ui/global.css";
 
-export const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
